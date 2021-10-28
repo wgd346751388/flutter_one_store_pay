32fcb6d5d95e1d40725c415f13c4e76048a2e9fd
吴国栋 <346751388@qq.com>
2021-10-28 11:40:14 +0800

    one store支付

--- lib/src/one_store.dart
+++ lib/src/one_store.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
 
@@ -53,49 +54,62 @@ class OneStore {
 
   ///初始化
   Future<void> enablePendingPurchases() async {
-  String val =   await _channel.invokeMethod(METHOD_ONE_STORE_ENABLE_PENDING);
+    String val = await _channel.invokeMethod(METHOD_ONE_STORE_ENABLE_PENDING);
   }
 
   ///查询产品信息
   ///[identifiers]代表产品id集合例：['com.price.299']
   Future<ProductDetails> queryProductDetails(List<String> identifiers) async {
-    dynamic data = await _channel.invokeMethod(METHOD_ONE_STORE_QUERY, {
-      ARGUMENT_KEY_PRODUCTS_IDS: identifiers,
-    });
-    return ProductDetails.fromJson(data.cast<String, dynamic>());
+    if (Platform.isAndroid) {
+      dynamic data = await _channel.invokeMethod(METHOD_ONE_STORE_QUERY, {
+        ARGUMENT_KEY_PRODUCTS_IDS: identifiers,
+      });
+      return ProductDetails.fromJson(data.cast<String, dynamic>());
+    }
+    return null;
   }
 
   ///调起内购
   ///[productId]代表产品id
   ///[productType]产品类型：[ProductType.all],[ProductType.inapp],[ProductType.auto]
   Future<ResultData> buyProduct(String productId, ProductType productType) async {
-    dynamic data = await _channel.invokeMethod<dynamic>(METHOD_ONE_STORE_BUY, {
-      ARGUMENT_KEY_PRODUCTS_ID: productId,
-      ARGUMENT_KEY_PRODUCTS_TYPE: ProductTypeConverter().toJson(productType),
-    });
-    return ResultData.fromJson(data.cast<String, dynamic>());
+    if (Platform.isAndroid) {
+      dynamic data = await _channel.invokeMethod<dynamic>(METHOD_ONE_STORE_BUY, {
+        ARGUMENT_KEY_PRODUCTS_ID: productId,
+        ARGUMENT_KEY_PRODUCTS_TYPE: ProductTypeConverter().toJson(productType),
+      });
+      return ResultData.fromJson(data.cast<String, dynamic>());
+    }
+
+    return null;
   }
 
   ///消耗型产品必须先消耗，否则无法进行下次内购
   Future<ResultData> consumePurchase(Purchase purchase) async {
-    dynamic data = await _channel.invokeMethod<dynamic>(METHOD_ONE_STORE_CONSUME, {
-      ARGUMENT_KEY_PURCHASE_STRING: purchase.originalJson,
-      ARGUMENT_KEY_PURCHASE_signature: purchase.signature,
-      ARGUMENT_KEY_PURCHASE_BILLING_KEY: purchase.billingKey,
-    });
-    return ResultData.fromJson(data.cast<String, dynamic>());
+    if (Platform.isAndroid) {
+      dynamic data = await _channel.invokeMethod<dynamic>(METHOD_ONE_STORE_CONSUME, {
+        ARGUMENT_KEY_PURCHASE_STRING: purchase.originalJson,
+        ARGUMENT_KEY_PURCHASE_signature: purchase.signature,
+        ARGUMENT_KEY_PURCHASE_BILLING_KEY: purchase.billingKey,
+      });
+      return ResultData.fromJson(data.cast<String, dynamic>());
+    }
+    return null;
   }
 
   ///非消耗性产品，请使用
   ///购买包月型商品时，仅对首次支付确认购买即可
   ///PurchaseData对象包含显示是否已确认购买的isAcknowledged
   Future<ResultData> acknowledgeAsync(Purchase purchase) async {
-    dynamic data = await _channel.invokeMethod<dynamic>(METHOD_ONE_STORE_ACKNOWLEDGE, {
-      ARGUMENT_KEY_PURCHASE_STRING: purchase.originalJson,
-      ARGUMENT_KEY_PURCHASE_signature: purchase.signature,
-      ARGUMENT_KEY_PURCHASE_BILLING_KEY: purchase.billingKey,
-    });
-    return ResultData.fromJson(data.cast<String, dynamic>());
+    if (Platform.isAndroid) {
+      dynamic data = await _channel.invokeMethod<dynamic>(METHOD_ONE_STORE_ACKNOWLEDGE, {
+        ARGUMENT_KEY_PURCHASE_STRING: purchase.originalJson,
+        ARGUMENT_KEY_PURCHASE_signature: purchase.signature,
+        ARGUMENT_KEY_PURCHASE_BILLING_KEY: purchase.billingKey,
+      });
+      return ResultData.fromJson(data.cast<String, dynamic>());
+    }
+    return null;
   }
 
   ///获取未消费的管理型商品和正在使用的包月型商品的信息。
@@ -104,9 +118,12 @@ class OneStore {
   /// 引用软件在后台时，用户可进行购买，因此从onResume Messude调用queryPastPurchases（）。
   ///[productType]产品类型：[ProductType.all],[ProductType.inapp],[ProductType.auto]
   Future<PurchaseData> queryPastPurchases(ProductType productType) async {
-    dynamic data = await _channel.invokeMethod<dynamic>(METHOD_ONE_STORE_QUERY_PURCHASES, {
-      ARGUMENT_KEY_PRODUCTS_TYPE: ProductTypeConverter().toJson(productType),
-    });
-    return PurchaseData.fromJson(data.cast<String, dynamic>());
+    if (Platform.isAndroid) {
+      dynamic data = await _channel.invokeMethod<dynamic>(METHOD_ONE_STORE_QUERY_PURCHASES, {
+        ARGUMENT_KEY_PRODUCTS_TYPE: ProductTypeConverter().toJson(productType),
+      });
+      return PurchaseData.fromJson(data.cast<String, dynamic>());
+    }
+    return null;
   }
 }
